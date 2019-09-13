b4c96d04f638eb296f06a8eed4f7832d474ce758
5HT <maxim@synrc.com>
2019-09-13 19:23:59 +0300

    mix

---
+++ mix.exs
@@ -0,0 +1,17 @@
+defmodule EXE.Mixfile do
+  use Mix.Project
+  def deps, do: [ {:ex_doc, ">= 0.0.0", only: :dev} ]
+  def application, do: [mod: {:sh, []}]
+  def project do
+    [ app: :exe,
+      version: "3.9.0",
+      description: "EXE Shell Execution",
+      package: package(),
+      deps: deps()]
+  end
+  def package do
+    [ files: ~w(man c_src src mix.exs rebar.config LICENSE),
+      licenses: ["ISC"],
+      links: %{"GitHub" => "https://github.com/synrc/exe"}]
+  end
+end
